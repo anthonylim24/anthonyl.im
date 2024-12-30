84d5c838b3606e9ff3b72cbf6962863afb2cc02a
Anthony Lim <anthonylim@ATHLAT10.plsinfo.org>
2024-12-29 16:23:27 -0800

    Figlet

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+import figlet from "figlet";
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-      return new Response("Bun!");
+      return new Response(figlet.textSync("Anthony Lim!"));
     },
   });
   
